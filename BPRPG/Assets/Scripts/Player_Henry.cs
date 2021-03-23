164df019b65b73d9db264b4c65bdddb3c2b423cd
Kevin Ponce <kev.ponce4@gmail.com>
2021-03-22 22:19:38 -0700

    combined code

--- BPRPG/Assets/Scripts/Player_Henry.cs
+++ BPRPG/Assets/Scripts/Player_Henry.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class Player_Henry : MonoBehaviour
 {
 
     #region attack_vars
--- BPRPG/Assets/Scripts/Player_scripts/Abby_Player.cs
+++ BPRPG/Assets/Scripts/Player_scripts/Abby_Player.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class Abby_Player : MonoBehaviour
 {
 
     #region attack_vars
