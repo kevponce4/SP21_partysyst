79c22656115deeaf0d29d25d88c42d660c4fd76e
Vivian Liu <vivizliu@gmail.com>
2021-03-24 19:37:42 -0700

    committing to pull

---
+++ BPRPG/Assets/Scripts/Enemy_scripts/HazardousObj.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HazardousObj : MonoBehaviour
+{
+    private int dmg;
+    private Vivian_Player player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        GameObject item = other.gameObject;
+
+        if(item.name == "Spikes1")
+        {
+            player = GetComponent<Vivian_Player>();
+            player.TakeDamage(dmg);
+        }
+    }
+}
