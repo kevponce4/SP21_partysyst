cbf242c89cc29ed17c9b494fd290b3addc267f35
Henry La <henryla106@gmail.com>
2021-04-11 01:01:25 -0700

    Created barriers, edited camera script, organized tutorial, and changed attack

--- BPRPG/Assets/Scripts/Henry_CameraScript.cs
+++ BPRPG/Assets/Scripts/Henry_CameraScript.cs
@@ -11,39 +11,39 @@ public class Henry_CameraScript : MonoBehaviour{
 
     // Start is called before the first frame update
     
-    void Start(){
-        threshold = calculateThreshold();
-        rb = followObject.GetComponent<Rigidbody2D>();
-    }
+    // void Start(){
+    //     threshold = calculateThreshold();
+    //     rb = followObject.GetComponent<Rigidbody2D>();
+    // }
 
-    // Update is called once per frame
-    void FixedUpdate(){
-        Vector2 follow = followObject.transform.position;
-        float xDifference = Vector2.Distance(Vector2.right * transform.position.x, Vector2.right * follow.x);
-        float yDifference = Vector2.Distance(Vector2.up * transform.position.y, Vector2.up * follow.y);
+    // // Update is called once per frame
+    // void FixedUpdate(){
+    //     Vector2 follow = followObject.transform.position;
+    //     float xDifference = Vector2.Distance(Vector2.right * transform.position.x, Vector2.right * follow.x);
+    //     float yDifference = Vector2.Distance(Vector2.up * transform.position.y, Vector2.up * follow.y);
 
-        Vector3 newPosition = transform.position;
-        if(Mathf.Abs(xDifference) >= threshold.x){
-            newPosition.x = follow.x;
-        }
-        if(Mathf.Abs(yDifference) >= threshold.y){
-            newPosition.y = follow.y;
-        }
-        float moveSpeed = rb.velocity.magnitude > speed ? rb.velocity.magnitude : speed;
-        transform.position = Vector3.MoveTowards(transform.position, newPosition, moveSpeed * Time.fixedDeltaTime);
-    }
-    private Vector3 calculateThreshold(){
-        Rect aspect = Camera.main.pixelRect;
-        Vector2 t = new Vector2(Camera.main.orthographicSize * aspect.width / aspect.height, Camera.main.orthographicSize);
-        t.x -= followOffset.x;
-        t.y -= followOffset.y;
-        return t;
-    }
+    //     Vector3 newPosition = transform.position;
+    //     if(Mathf.Abs(xDifference) >= threshold.x){
+    //         newPosition.x = follow.x;
+    //     }
+    //     if(Mathf.Abs(yDifference) >= threshold.y){
+    //         newPosition.y = follow.y;
+    //     }
+    //     float moveSpeed = rb.velocity.magnitude > speed ? rb.velocity.magnitude : speed;
+    //     transform.position = Vector3.MoveTowards(transform.position, newPosition, moveSpeed * Time.fixedDeltaTime);
+    // }
+    // private Vector3 calculateThreshold(){
+    //     Rect aspect = Camera.main.pixelRect;
+    //     Vector2 t = new Vector2(Camera.main.orthographicSize * aspect.width / aspect.height, Camera.main.orthographicSize);
+    //     t.x -= followOffset.x;
+    //     t.y -= followOffset.y;
+    //     return t;
+    // }
 
-    // Box limit
-    private void OnDrawGizmos() {
-        Gizmos.color = Color.blue;
-        Vector2 border = calculateThreshold();
-        Gizmos.DrawWireCube(transform.position, new Vector3(border.x * 2, border.y * 2, 1));
-    }
+    // // Box limit
+    // private void OnDrawGizmos() {
+    //     Gizmos.color = Color.blue;
+    //     Vector2 border = calculateThreshold();
+    //     Gizmos.DrawWireCube(transform.position, new Vector3(border.x * 2, border.y * 2, 1));
+    // }
 }
