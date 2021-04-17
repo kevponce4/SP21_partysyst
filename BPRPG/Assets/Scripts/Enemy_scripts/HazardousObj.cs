ce2420af096717f8547a1aaae4d0fab08bf0f57a
Vivian Liu <vivizliu@gmail.com>
2021-04-17 10:17:44 -0700

    added pushable boxes

--- BPRPG/Assets/Scripts/Enemy_scripts/HazardousObj.cs
+++ BPRPG/Assets/Scripts/Enemy_scripts/HazardousObj.cs
@@ -5,25 +5,58 @@ using UnityEngine;
 public class HazardousObj : MonoBehaviour
 {
     [SerializeField]
-    private int dmg;
+    private int spikeDmg;
+
+    [SerializeField]
+    private int bombDmg;
+
+    private SphereCollider myCollider;
+    public Transform m_particles;
+    private float time = 3;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        myCollider = transform.GetComponent<SphereCollider>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        StartCoroutine(PlaceBomb());
+
+    }
+
+    IEnumerator PlaceBomb() {
+        yield return new WaitForSeconds(time); // Delay execution for time seconds
+    
+        if (myCollider.radius < 0.02f)
+        {
+    
+            Instantiate(m_particles, transform.position, transform.rotation);
+            myCollider.radius += 0.005f;
+    
+        }
+        /*else
+        {
+            myCollider.enabled = false;
+            //Destroy(gameObject);
+        }*/
     }
 
     private void OnCollisionEnter2D(Collision2D other)
     {
         if (other.transform.CompareTag("Player"))
         {
-            other.transform.GetComponent<Player>().TakeDamage(dmg);
+            other.transform.GetComponent<Player>().TakeDamage(spikeDmg);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.CompareTag("Player"))
+        {
+            other.transform.GetComponent<Player>().TakeDamage(bombDmg);
         }
     }
 }
