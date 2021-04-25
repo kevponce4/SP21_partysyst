9a7938c7ea622e021add8be08005e35cc2cde602
Abigail <abigaildelgado@berkeley.edu>
2021-04-25 16:29:43 -0700

    finished abby_level and added nextLevel prefab to transition between levels

---
+++ BPRPG/Assets/Scripts/NextLevel.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NextLevel : MonoBehaviour
+{
+    [SerializeField] [Tooltip("set this to scene number you want (0 - tutorial, 1 - jane, 2 - abby")]
+    private int sceneNum;
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.transform.CompareTag("Player"))
+        {
+            if (sceneNum == 0) {
+                GameManager.Instance.TutorialLevel();
+            }
+            if (sceneNum == 1) {
+                GameManager.Instance.JaneLevel();
+            }
+            if (sceneNum == 2) {
+                GameManager.Instance.AbbyLevel();
+            }
+        }
+    }
+}
